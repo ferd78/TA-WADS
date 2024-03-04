1cc4054fe2fdaaae0c1f2129dd99de4ee0b4f7b2
ferd78 <114067344+ferd78@users.noreply.github.com>
2024-03-05 00:02:26 +0700

    Update TodoItem.jsx

--- components/TodoItem.jsx
+++ components/TodoItem.jsx
@@ -9,10 +9,10 @@ export function TodoItem({ completed, id, title, toggleTodo, deleteTodo, editTod
             onChange={(e) => toggleTodo(id, e.target.checked)}
           />
           {title}
-          <button className="btn btn-success" onClick={() => editTodo(id)}>
+          <button className="btn btn-success hover:bg-green-500 hover:bg-opacity-20 focus-visible:bg-green-500 focus-visible:bg-opacity-20" onClick={() => editTodo(id)}>
             Edit Item
           </button>
-          <button className="btn btn-danger" onClick={() => deleteTodo(id)}>
+          <button className="btn btn-danger hover:bg-red-500 hover:bg-opacity-20 focus-visible:bg-red-500 focus-visible:bg-opacity-20" onClick={() => deleteTodo(id)}>
            Delete Item
           </button>
         </label>
